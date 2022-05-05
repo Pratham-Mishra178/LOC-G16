d284e30f6ece1c1ab49741aa21df46f184edc30c
subhasmitasahoo9337 <99581996+subhasmitasahoo9337@users.noreply.github.com>
2022-05-05 20:37:02 +0530

    Add files via upload

---
+++ Week-3/Subhasmita/W3Q4.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+int main ()
+void swap(int,int);
+{
+int a,b;
+printf ("enter values for a and b\n");
+scanf("%d%d",&a,&b);
+printf("\n\nBefore swapping:a=%d and b=%d\n,a,b");
+return 0;
+}
